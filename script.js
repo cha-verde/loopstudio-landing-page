175856d4f358784a2837aed4cfd3569e9ef17c3b
cha-verde <novitrna@gmail.com>
2025-10-28 10:35:31 +0700

    Finish mobile layout

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+
+const header = document.querySelector("header")
+const navigationHamburger = document.querySelector(".navigation-hamburger")
+const navigationMobile = document.querySelector(".mobile")
+const closeNavigationMobile = document.querySelector(".mobile__close")
+const headerHero = document.querySelector(".hero__mobile")
+const mobileLinks = document.querySelector(".mobile__links")
+
+
+
+const mobileNavigationClose = document.querySelector(".mobile__close")
+
+navigationHamburger.addEventListener('click', () => {
+    document.body.style.overflow = 'hidden';
+    mobileLinks.style.display = "block";
+    mobileLinks.style.position = "absolute";
+})
+
+
+mobileNavigationClose.addEventListener('click', () => {
+    header.style.display = "block";
+    navigationMobile.style.display = 'none';
+    document.body.style.overflow = 'visible';
+    navigationMobile.style.display = "block";
+    mobileLinks.style.display = "none";
+})
+
+
 const card = document.querySelectorAll("div.card")
 
 const desktopImages = [
@@ -16,10 +44,55 @@ card.forEach(element => {
     const img = a.querySelector("img");
     const name = img.getAttribute("file");      
     desktopImages.forEach(i => {
-        console.log(i);
-        console.log(name);
         if(i.includes(name)){
             img.src = i;  
         }
     })
 });
+
+const mobileImages = [
+    "./images/mobile/cards/image-curiosity.jpg",
+    "./images/mobile/cards/image-deep-earth.jpg",
+    "./images/mobile/cards/image-fisheye.jpg",
+    "./images/mobile/cards/image-from-above.jpg",
+    "./images/mobile/cards/image-grid.jpg",
+    "./images/mobile/cards/image-night-arcade.jpg",
+    "./images/mobile/cards/image-pocket-borealis.jpg",
+    "./images/mobile/cards/image-soccer-team.jpg"
+]
+
+
+function changeHeader(media){
+    if(media.matches){
+        card.forEach(element => {
+            const a = element.querySelector("a");
+            const img = a.querySelector("img");
+            const name = img.getAttribute("file");      
+            mobileImages.forEach(i => {
+                if(i.includes(name)){
+                    img.src = i;  
+                }
+            })
+        });
+    }
+
+    else{
+        card.forEach(element => {
+            const a = element.querySelector("a");
+            const img = a.querySelector("img");
+            const name = img.getAttribute("file");      
+            desktopImages.forEach(i => {
+                if(i.includes(name)){
+                    img.src = i;  
+                }
+            })
+        });
+        
+    }
+}
+
+const media = window.matchMedia("(width < 800px)")
+
+media.addEventListener("change", () => {
+    changeHeader(media);
+})
